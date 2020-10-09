970b783ba75597d117992902e289b5d91dab269f
liubo <2281075105@qq.com>
2020-10-09 10:05:43 +0800

    闪屏页面

--- app/src/main/java/com/itheima/zhbj/SplashActivity.java
+++ app/src/main/java/com/itheima/zhbj/SplashActivity.java
@@ -1,14 +1,20 @@
 package com.itheima.zhbj;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
+public class SplashActivity extends Activity {
 
-public class MainActivity extends Activity {
+    private RelativeLayout rl_root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        rl_root = findViewById(R.id.rl_root);
+
+
     }
 }
