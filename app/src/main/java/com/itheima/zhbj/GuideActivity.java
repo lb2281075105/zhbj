5daa1f2d8616bf643e91aaf555c3e7a857980e5b
liubo <2281075105@qq.com>
2020-10-10 13:51:51 +0800

    ...

--- app/src/main/java/com/itheima/zhbj/GuideActivity.java
+++ app/src/main/java/com/itheima/zhbj/GuideActivity.java
@@ -2,6 +2,7 @@ package com.itheima.zhbj;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 
 public class GuideActivity extends Activity {
@@ -9,6 +10,9 @@ public class GuideActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 必须在setContentView之前调用
+//        requestWindowFeature(Window.FEATURE_NO_TITLE); // 去掉标题,
+
         setContentView(R.layout.activity_guide);
     }
 }
--- app/src/main/java/com/itheima/zhbj/MainActivity.java
+++ app/src/main/java/com/itheima/zhbj/MainActivity.java
@@ -2,6 +2,9 @@ package com.itheima.zhbj;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends Activity {
